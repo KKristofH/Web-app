6754512cc3118fa5fdb2ac7a01dec73c7bde4618
KKristofH <130987956+KKristofH@users.noreply.github.com>
2026-03-12 17:49:26 +0100

    Update cache version and clean up fetch event listener

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'galeria-cache-v13';
+const CACHE_NAME = 'galeria-cache-v15';
 const urlsToCache = [
   './',
   './index.html',
@@ -32,19 +32,14 @@ self.addEventListener('activate', event => {
   );
 });
 
-// Agresszív offline stratégia
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
-      // 1. Ha megvan a fájl a memóriában, azonnal adjuk vissza (OFFLINE mód)
       if (cachedResponse) {
         return cachedResponse;
       }
       
-      // 2. Ha nincs meg, megpróbáljuk letölteni a netről
       return fetch(event.request).catch(() => {
-        // 3. HA A NET HALOTT (vagy repülőn vagy), és egy oldalt próbál betölteni az iOS, 
-        // erőszakkal adjuk vissza neki az index.html-t.
         if (event.request.mode === 'navigate' || event.request.headers.get('accept').includes('text/html')) {
           return caches.match('./index.html');
         }
